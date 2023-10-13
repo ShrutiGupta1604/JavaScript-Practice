eab8d309fc88cdda578ad9a42da050a1b0f66feb
ShrutiGupta <ShrutiGupta1604@gmail.com>
2023-10-13 13:16:35 +0530

    for loop

---
+++ JSExamples.js
@@ -0,0 +1,9 @@
+
+//-----------------------------------------------------------
+for(var conditionCheck = 0; conditionCheck<=3; conditionCheck++)
+{
+	for(var tempconditionCheck = 0 ; tempconditionCheck<=conditionCheck; tempconditionCheck++)
+	{
+		console.log("*") 
+	}
+}
