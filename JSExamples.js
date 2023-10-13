713a802607cfc91c39ce91705e31599c4028212c
ShrutiGupta <ShrutiGupta1604@gmail.com>
2023-10-13 14:11:40 +0530

    Star Pattern

--- JSExamples.js
+++ JSExamples.js
@@ -4,6 +4,10 @@ for(var conditionCheck = 0; conditionCheck<=3; conditionCheck++)
 {
 	for(var tempconditionCheck = 0 ; tempconditionCheck<=conditionCheck; tempconditionCheck++)
 	{
-		console.log("*") 
-	}
+		//console.log("*") 
+		process. stdout. write("*")
+		 
+		 
+	}  console.log("\n") 
 }
+ 
